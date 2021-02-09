9a48d4e59fc9de62ef9193fe877f1b75362f0a25
jamesrotimi <jamesrotimi@icloud.com>
2021-02-09 10:37:09 +0000

    First push

---
+++ src/main/java/com/example/StockTicker/StockTickerApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockTicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockTicker/StockTickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockTicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
